2772051b1fd415734088ad750ca6d868c02f9ba6
José Luis Gutiérrez <jlgutierrez@ugr.es>
2014-05-19 17:06:38 +0200

    Add Clean Buildroot
    Adding a clean version of the git Buildroot repository

---
+++ buildroot/package/rpi-userland/rpi-userland.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# rpi-userland
+#
+################################################################################
+
+RPI_USERLAND_VERSION = eccb81050afd177da1923404b366c6226f29bfe0
+RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
+RPI_USERLAND_LICENSE = BSD-3c
+RPI_USERLAND_LICENSE_FILES = LICENCE
+RPI_USERLAND_INSTALL_STAGING = YES
+RPI_USERLAND_CONF_OPT = -DVMCS_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=OFF
+
+define RPI_USERLAND_POST_TARGET_CLEANUP
+    rm -Rf $(TARGET_DIR)/usr/src
+endef
+RPI_USERLAND_POST_INSTALL_TARGET_HOOKS += RPI_USERLAND_POST_TARGET_CLEANUP
+
+$(eval $(cmake-package))
