9d2492ccd74388248fffb7f8f1dc2fd54d4efae6
José Luis Gutiérrez <jlgutierrez@ugr.es>
2014-05-23 11:23:14 +0200

    Adding Ethernet Fix & lighttpd config file
    Adding:
       - Ethernet Device-tree register fix: wrong reg=<0x7> --> reg=<0x0>
    	- added in copy_to_tftpfolder.sh script.
       - lighttpd configuration file

--- buildroot/copy_to_tftpfolder.sh
+++ buildroot/copy_to_tftpfolder.sh
@@ -4,6 +4,11 @@ echo Deleting old files
 rm ~/tftpboot/devicetree.dtb 
 rm ~/tftpboot/uImage
 rm ~/tftpboot/uramdisk.image.gz
+echo Fixing Ethernet Register Device-Tree
+dtc -I dtb -O dts -o output/images/zynq-zed.dts output/images/zynq-zed.dtb
+sed -i 's/<0x7>/<0x0>/g' output/images/zynq-zed.dts
+rm output/images/zynq-zed.dtb
+dtc -I dts -O dtb -o output/images/zynq-zed.dtb output/images/zynq-zed.dts
 echo Copying uImage uRamdisk and  Device-Tree to ~/tftpfolder
 cp output/images/zynq-zed.dtb ~/tftpboot/devicetree.dtb
 cp output/images/uImage ~/tftpboot/uImage
