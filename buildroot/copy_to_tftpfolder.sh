cc8ebf139eef8619f3c7e9bdc23f345967ecccf5
José Luis Gutiérrez <jlgutierrez@ugr.es>
2014-07-23 13:11:41 +0200

    tftpscript destination folder fix
    adding variables for destination folder

--- buildroot/copy_to_tftpfolder.sh
+++ buildroot/copy_to_tftpfolder.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+DEST_FOLDER=/home/jlgutierrez/zedboot
+echo Destination folder is $DEST_FOLDER
 echo Deleting old files
-rm ~/tftpboot/devicetree.dtb 
-rm ~/tftpboot/uImage
-rm ~/tftpboot/uramdisk.image.gz
+rm $DEST_FOLDER/devicetree.dtb 
+rm $DEST_FOLDER/uImage
+rm $DEST_FOLDER/uramdisk.image.gz
 echo Fixing Ethernet Register Device-Tree
 dtc -I dtb -O dts -o output/images/zynq-zed.dts output/images/zynq-zed.dtb
 sed -i 's/<0x7>/<0x0>/g' output/images/zynq-zed.dts
 rm output/images/zynq-zed.dtb
 dtc -I dts -O dtb -o output/images/zynq-zed.dtb output/images/zynq-zed.dts
-echo Copying uImage uRamdisk and  Device-Tree to ~/tftpfolder
-cp output/images/zynq-zed.dtb ~/tftpboot/devicetree.dtb
-cp output/images/uImage ~/tftpboot/uImage
-cp output/images/rootfs.cpio.uboot ~/tftpboot/uramdisk.image.gz
+echo Copying uImage uRamdisk and Device-Tree to $DEST_FOLDER
+cp output/images/zynq-zed.dtb $DEST_FOLDER/devicetree.dtb
+cp output/images/uImage $DEST_FOLDER/uImage
+cp output/images/rootfs.cpio.uboot $DEST_FOLDER/uramdisk.image.gz
 echo Finished...
 
