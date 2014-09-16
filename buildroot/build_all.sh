576a5353faa46afaceb6d0a40a55fc831a75fef3
José Luis Gutiérrez <jlgutierrez@ugr.es>
2014-09-16 19:25:07 +0200

    buidlroot: build_all.sh to build everthing
    first stage. It must:
    - compile buildroot
    - export LINUX, CROSS_COMPILE & ARCH
    - compile WRS kernel drivers (if so)
    - compile WRS userspace (if so)
    - compile spec-sw kernel drivers (ongoing)
    -- copy spec folders *.ko to output/target/wr/lib/modules (ongoing)

---
+++ buildroot/build_all.sh
@@ -0,0 +1,161 @@
+#!/bin/sh
+########################################################################
+#  compile_userspace.sh
+# 
+# blablabla
+# 
+#   Created on: Sep 1, 2014
+#   Authors:
+#  		- José Luis Gutiérrez
+#	Institutions: 
+#		- University of Granada
+########################################################################
+
+USERSPACE=userspace
+BUILDROOT=$PWD
+WR_INSTALL_ROOT=$PWD/output/target/wr
+## Let's build userspace ##
+
+## Set ARCH & CROSS_COMPILE
+export ARCH=arm
+
+# Check own buildroot cross-compiler exists
+
+if [ -f output/host/usr/bin/arm-linux-gcc ]; then
+export CROSS_COMPILE=$PWD/output/host/usr/bin/arm-linux-
+else 
+	echo -e "\e[91mError: arm-linux-gcc does not exist in buildroot folder\e[39m"
+	echo -e "\e[91mPlease compile buildroot first\e[39m"
+	echo "Running make to generate buildroot cross-compiler"
+	make
+	export CROSS_COMPILE=$PWD/output/host/usr/bin/arm-linux-
+fi
+
+#Prueba
+# Compilador cruzado del buildroot de WRS
+# LINUX headers del WRS
+export CROSS_COMPILE=/home/jlgutierrez/wr/wr-switch-zynq/buildroot/output/host/usr/bin/arm-linux-
+export LINUX=/home/jlgutierrez/wr/wr-switch-zynq/buildroot/output/build/linux-3.14.4
+
+echo "*********************************************************************"
+echo "**** Compiling userspace with the following parameters:"
+echo "**** ARCH=$ARCH"
+echo "**** CROSS_COMPILE=$CROSS_COMPILE"
+echo "**** WR_INSTALL_ROOT=$WR_INSTALL_ROOT"
+echo "**** LINUX=$LINUX"
+echo "*********************************************************************"
+
+
+sleep 5
+
+## We clean all ##
+cd $PWD/$USERSPACE
+make clean
+cd $BUILDROOT
+
+### Apply patches to kernel ##
+#echo "Applying kernel patches"
+#patchdir="$BUILDROOT/patches/kernel/v2.6.39"
+
+## go to the build dir and compile it, using our configuration
+#cd $PWD/output/build
+#dirname="linux-xilinx-v14.5"
+## apply patches
+#cd $dirname
+#for n in ${patchdir}/00*; do
+    #patch -p1 < $n || wrs_die "patch kernel"
+#done
+#cd $BUILDROOT
+#echo "Kernel patches applied"
+#sleep 5
+
+## Compile Kernel Drivers ###
+echo -e "\e[92mCompiling kernel drivers\e[39m"
+cd $PWD/kernel
+make clean
+make all
+echo -e "\e[92mKernel drivers compiled\e[39m"
+sleep 5
+
+#Copy .ko by hand
+echo -e "\e[92mCopying kernel drivers to target filesystem\e[39m"
+mkdir -p $WR_INSTALL_ROOT/lib/modules
+cp at91_softpwm/*.ko $WR_INSTALL_ROOT/lib/modules
+cp wr_nic/*.ko $WR_INSTALL_ROOT/lib/modules
+cp wr_pstats/*.ko $WR_INSTALL_ROOT/lib/modules
+cp wr_rtu/*.ko $WR_INSTALL_ROOT/lib/modules
+cp wr_vic/*.ko $WR_INSTALL_ROOT/lib/modules
+cd $BUILDROOT
+echo -e "\e[92mKernel drivers copied\e[39m"
+sleep 5
+
+
+# subdirectories we want to compile
+cd $PWD/$USERSPACE/libptpnetif
+make clean
+make
+cd $BUILDROOT
+echo -e "\e[92mlibptpnetif built\e[39m"
+sleep 5
+
+
+cd $PWD/$USERSPACE/mini-rpc
+make clean
+make
+cd $BUILDROOT
+echo -e "\e[92mmini-rpc built\e[39m"
+sleep 5
+
+
+cd $PWD/$USERSPACE/libswitchhw
+make clean
+make
+cd $BUILDROOT
+echo -e "\e[92mlibswitchhw  built\e[39m"
+sleep 5
+
+
+cd $PWD/$USERSPACE/wrsw_hal
+make clean
+make
+cd $BUILDROOT
+echo -e "\e[92mwrsw_hal built\e[39m"
+sleep 5
+
+cd $PWD/$USERSPACE/wrsw_rtud
+make clean
+make
+cd $BUILDROOT
+echo -e "\e[92mwrsw_rtud built\e[39m"
+sleep 5
+
+cd $PWD/$USERSPACE/ptp-noposix
+make clean
+make
+cd $BUILDROOT
+echo -e "\e[92mptp-noposix built\e[39m"
+sleep 5
+
+cd $PWD/$USERSPACE/tools
+make clean
+make 
+make install
+cd $BUILDROOT
+echo -e "\e[92mtools built\e[39m"
+sleep 5
+
+#Let's now copy binaries to the generated buildroot filesystem#
+echo "do not forget to copy binaries"
+echo "make buildroot again"
+
+## Install (copy) drivers .ko, binaries and libraries to target/
+cd $PWD/$USERSPACE/
+make install
+cd $BUILDROOT
+
+## Re-Generate root filesystem
+make 
+
+## Run copy_to_tftpfolder 
+./copy_to_tftpfolder.sh
+
